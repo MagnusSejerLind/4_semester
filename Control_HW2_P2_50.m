3412aaad93cd5fb9227c7946fa8ce53d9d8dd86c
MagnusSejerLind <82442041+MagnusSejerLind@users.noreply.github.com>
2022-02-22 14:58:44 +0100

    Add files via upload

---
+++ Control_HW2_P2_50.m
@@ -0,0 +1,23 @@
+
+% Numerator and denominator of transfer function
+num = [0 0 14000];
+den = [1 45 3100 14500];
+
+% Express transfer function
+TF = tf(num,den);
+
+% Determine poles and zeros
+P = pole(TF);
+Z = zero(TF);
+
+figure(1)
+% Pole & Zero plot of system
+pzplot(TF)
+
+grid
+a = findobj(gca,'type','line');
+for i = 1:length(a)
+    set(a(i),'markersize',10) %change marker size
+    set(a(i), 'linewidth',2)  %change linewidth
+end
+
